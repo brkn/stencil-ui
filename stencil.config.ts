43db2ec1258b67d39b41eca5f81fcd1e15b2b64b
Berkan Unal <Berkanunal@gmail.com>
2021-02-01 08:59:49 +0300

    Fix stencil config

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,21 +1,39 @@
 import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'stencil-ui',
+  plugins: [sass()],
   outputTargets: [
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
     {
       type: 'dist-custom-elements-bundle',
     },
     {
       type: 'docs-readme',
+      dir: 'docs',
     },
     {
+      /**
+       * This is for building an app with stencil.
+       *
+       * Eventhough we dont need an app, this is useful to build out and test the components
+       * throughout development.
+       */
       type: 'www',
       serviceWorker: null, // disable service workers
     },
   ],
+  testing: {
+    browserArgs: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--headless',
+      '--disable-gpu',
+      '--disable-dev-shm-usage',
+      '--disable-software-rasterizer',
+      '--mute-audio',
+      '--remote-debugging-port=0',
+      '--window-size=1440,900',
+    ],
+  },
 };
